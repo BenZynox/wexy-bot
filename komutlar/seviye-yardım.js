d23a3b25363d1bc2eb1de4affc99e23e6ebea7d1
BenZynox <104078510+BenZynox@users.noreply.github.com>
2022-04-26 15:32:37 +0300

    Update seviye-yardım.js

--- komutlar/seviye-yardım.js
+++ komutlar/seviye-yardım.js
@@ -2,34 +2,34 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 const talkedRecently = new Set();
 const db = require("quick.db");
-let botid = ('811238807692771348') 
+let botid = ('960536191856410624') 
 exports.run = async(client, message, args) => {
  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!"; 
     const embed = new Discord.MessageEmbed()
         .setAuthor(`${client.user.username} `, client.user.displayAvatarURL({dynamic: true}))
         .setColor('#d02090')
-       .setTitle(`**Wexy Bot Seviye Menüsüne Hoşgeldiniz**`)
+       .setTitle(`**Jarvıs Bot Seviye Menüsüne Hoşgeldiniz**`)
         .setDescription(`
  
-  **» ${prefix}seviye** Mevcut Olduğunuz Seviyeyi Gösterir.
-  **» ${prefix}seviye-ayarlar** Sunucuda Aktif Olan Seviye Ayarlarını Gösterir.
-  **» ${prefix}seviye-rol**  İstenilen Seviyeye Gelince Verilecek Rolü Ayarlar.
-  **» ${prefix}seviye-sıfırla ** Mevcut Seviye Sistemini Sıfırlar
-  **» ${prefix}seviye-sınır ** Maksimum Kazanılanabilecek Seviyeyi Belirler.
-  **» ${prefix}seviye-xp **  Bir Mesaj Başına Verilecek Xp yi ayarlar.
-  **» ${prefix}seviye-top ** Sunucuda ki En yüksek 5 Kişiyi Gösterir
-  **» ${prefix}seviye-rütbeler ** Hangi Seviye de Rol Verilecek Onu Gösterir.
+  **» -seviye** Mevcut Olduğunuz Seviyeyi Gösterir.
+  **» -seviye-ayarlar** Sunucuda Aktif Olan Seviye Ayarlarını Gösterir.
+  **» -seviye-rol**  İstenilen Seviyeye Gelince Verilecek Rolü Ayarlar.
+  **» -seviye-sıfırla ** Mevcut Seviye Sistemini Sıfırlar
+  **» -seviye-sınır ** Maksimum Kazanılanabilecek Seviyeyi Belirler.
+  **» -seviye-xp **  Bir Mesaj Başına Verilecek Xp yi ayarlar.
+  **» -seviye-top ** Sunucuda ki En yüksek 5 Kişiyi Gösterir
+  **» -seviye-rütbeler ** Hangi Seviye de Rol Verilecek Onu Gösterir.
 ▬▬▬▬▬▬▬▬ \`\`\Genel Komutlar\`\`\ ▬▬▬▬▬▬▬▬
 
-**»  ${prefix}davet __Botu Davet Edebilirsiniz!__**
-**»  ${prefix}sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
-**»  ${prefix}istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
-**»  ${prefix}prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
-**»  ${prefix}prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
+**»  -davet __Botu Davet Edebilirsiniz!__**
+**»  -sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
+**»  -istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
+**»  -prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
+**»  -prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
 
 `)
-        .setThumbnail(`https://cdn.discordapp.com/attachments/735925634336817283/762342094554791936/ezgif.com-optimize_8-1.gif`)
-             .addField(`» Wexy Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=811238807692771348&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/FQVadvq) **|** [Oy Linki](http://bit.ly/Quinoy) **|** `)
+        .setThumbnail(``)
+             .addField(`» Jarvıs Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=966324540768657478&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/22YqX8sVRg) **|** [Oy Linki](http://bit.ly/Quinoy) **|** `)
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.displayAvatarURL({dynamic: true}))
     return message.channel.send(embed);
   

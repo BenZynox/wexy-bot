ac8ffac8966411e5f4fe035dd2eeb03e5321ba70
BenZynox <104078510+BenZynox@users.noreply.github.com>
2022-04-26 15:40:31 +0300

    Update yardım-mutesistem.js

--- komutlar/yardım-mutesistem.js
+++ komutlar/yardım-mutesistem.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 const ayarlar = require('../ayarlar.json');
 const talkedRecently = new Set();
-let botid = ('811238807692771348') 
+let botid = ('966324540768657478') 
  
 exports.run = async(client, message, args) => { 
   let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
@@ -10,25 +10,25 @@ exports.run = async(client, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setAuthor(`${client.user.username} `, client.user.displayAvatarURL({dynamic: true}))
         .setColor('#d02090')
-       .setTitle(` **Wexy Bot Yardım Menüsüne Hoşgeldiniz** `)
+       .setTitle(` **Jarvıs Bot Yardım Menüsüne Hoşgeldiniz** `)
         .setDescription(`
-  **» ${prefix}mute-ayarla**   Mute Sistemini Ayarlarsınız.
-  **» ${prefix}mute**   Mute Atarsınız.
-  **» ${prefix}süresiz-mute** Süresiz Mute Atarsınız.		
-  **» ${prefix}unmute** Etiketlediğiniz Kişinin Mutesini Kaldırırsınız.		
+  **» -mute-ayarla**   Mute Sistemini Ayarlarsınız.
+  **» -mute**   Mute Atarsınız.
+  **» -süresiz-mute** Süresiz Mute Atarsınız.		
+  **» -unmute** Etiketlediğiniz Kişinin Mutesini Kaldırırsınız.		
 
 
 
 ▬▬▬▬▬▬▬▬ \`\`\Genel Komutlar\`\`\ ▬▬▬▬▬▬▬▬
 
-**»  ${prefix}davet __Botu Davet Edebilirsiniz!__**
-**»  ${prefix}sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
-**»  ${prefix}istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
-**»  ${prefix}prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
-**»  ${prefix}prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
+**»  -davet __Botu Davet Edebilirsiniz!__**
+**»  -sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
+**»  -istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
+**»  -prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
+**»  -prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
 
 `)
-        .addField(`» Wexy Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=811238807692771348&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/yaknda) **|** [Oy Linki](http://bit.ly/yaknda) **|** `)
+        .addField(`» Jarvıs Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=966324540768657478&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/22YqX8sVRg) **|** [Oy Linki](http://bit.ly/yaknda) **|** `)
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.displayAvatarURL({dynamic: true}))
     return  message.channel.send(embed);
   

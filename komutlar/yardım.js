201bca8b91e3a47b0c0c744067374498ddc08987
BenZynox <104078510+BenZynox@users.noreply.github.com>
2022-04-26 15:35:15 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 const ayarlar = require('../ayarlar.json');
 const talkedRecently = new Set();
-let botid = ('811238807692771348') 
+let botid = ('966324540768657478') 
  
 exports.run = async(client, message, args) => { 
   let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
@@ -10,30 +10,30 @@ exports.run = async(client, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setAuthor(`${client.user.username} `, client.user.displayAvatarURL({dynamic: true}))
         .setColor('#d02090')
-       .setTitle(` **Wexy Bot Yardım Menüsüne Hoşgeldiniz** `)
+       .setTitle(` **Jarvıs Bot Yardım Menüsüne Hoşgeldiniz** `)
         .setDescription(`
-  **» ${prefix}premium-sistemi**   Premium Komutlarını Gösterir.		
-  **» ${prefix}ekonomi**  Ekonomi Komutlarını Gösterir.
-  **» ${prefix}gif-menü **   Gif Komutlarını Gosterir.
-  **» ${prefix}moderasyon** Moderasyon Komutlarını Gösterir.
-  **» ${prefix}moderasyon2**   2. Moderasyon Komutlarını Gösterir.
-  **» ${prefix}kullanıcı** Kullanıcı Komutlarıni Gösterir.
-  **» ${prefix}eğlence ** Eğlence Komutlarını Gösterir.
-  **» ${prefix}seviye-yardım **   Seviye Komutlarını Gösterir.
-  **» ${prefix}kayıt-sistemi**  Kayıt Komutlarını Gösterir.
-  **» ${prefix}koruma-yardım**  Koruma Komutlarını Gösterir.
-  **» ${prefix}mute-yardım**  Mute Komutlarını Gösterir.
+  **» -premium-sistemi**   Premium Komutlarını Gösterir.		
+  **» -ekonomi**  Ekonomi Komutlarını Gösterir.
+  **» -gif-menü **   Gif Komutlarını Gosterir.
+  **» -moderasyon** Moderasyon Komutlarını Gösterir.
+  **» -moderasyon2**   2. Moderasyon Komutlarını Gösterir.
+  **» -kullanıcı** Kullanıcı Komutlarıni Gösterir.
+  **» -eğlence ** Eğlence Komutlarını Gösterir.
+  **» -seviye-yardım **   Seviye Komutlarını Gösterir.
+  **» -kayıt-sistemi**  Kayıt Komutlarını Gösterir.
+  **» -koruma-yardım**  Koruma Komutlarını Gösterir.
+  **» -mute-yardım**  Mute Komutlarını Gösterir.
 
 ▬▬▬▬▬▬▬▬ \`\`\Genel Komutlar\`\`\ ▬▬▬▬▬▬▬▬
 
-**»  ${prefix}davet __Botu Davet Edebilirsiniz!__**
-**»  ${prefix}sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
-**»  ${prefix}istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
-**»  ${prefix}prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
-**»  ${prefix}prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
+**»  -davet __Botu Davet Edebilirsiniz!__**
+**»  -sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
+**»  -istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
+**»  -prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
+**»  -prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
 
 `)
-        .addField(`» Wexy Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=811238807692771348&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/yaknda) **|** [Oy Linki](http://bit.ly/yaknda) **|** `)
+        .addField(`» Jarvıs Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=966324540768657478&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/22YqX8sVRg) **|** [Oy Linki](http://bit.ly/yaknda) **|** `)
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.displayAvatarURL({dynamic: true}))
     return  message.channel.send(embed);
   

296724ff451213145a4855545490dd0a7015a1ac
BenZynox <104078510+BenZynox@users.noreply.github.com>
2022-04-26 15:24:46 +0300

    Update moderasyon.js

--- komutlar/moderasyon.js
+++ komutlar/moderasyon.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 const db = require('quick.db');
 const talkedRecently = new Set();
-let botid = ('811238807692771348') 
+let botid = ('966324540768657478') 
 exports.run = async(client, message, args) => {
   let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";  
     const embed = new Discord.MessageEmbed()
@@ -10,29 +10,29 @@ exports.run = async(client, message, args) => {
         .setColor('#d02090')
        .setTitle(` **Wexy Bot Moderasyon Menüsüne Hoşgeldiniz** `)
         .setDescription(` ${prefix}moderasyon2 Menümüze Bakmayı Unutmayın. 
-  **» ${prefix}giriş-çıkış-ayarla **  Resimli Hg-BB sistemini Açarsınız.
-  **» ${prefix}giriş-çıkış-kapat **  Ayarlanan Resimli Hg-BB Sistemini Kapatırsınız.
-  **» ${prefix}güvenlik **  Resimli Güvenlik Sistemini Belirlediğiniz Kanal Yapar..
-  **» ${prefix}güvenlik sıfırla **   Resimli Güvenlik Sistemini Kapatırsınız.
-  **» ${prefix}capslock-engelleme **   CapsLock Engelleme Sistemini Açıp Kapatırsınız (İlk Yazışta Açar 2.de kapar)
-  **» ${prefix}küfürengel**  Küfür Sistemini Açar/Kapatırsınız.
-  **» ${prefix}reklamengel** Reklam Engel Sistemini Açar/Kapatırsınız.
-  **» ${prefix}temizle ** Belirlenen Miktarda Mesaj Siler.
-  **» ${prefix}sunucupanel **   Sunucu Panel Açar.
-  **» ${prefix}unban ** İdsi Girelen Kullanıcıyı Banının Açar.
-  **» ${prefix}nuke ** Komutun Yazıldığı Kanalı Silip Aynı İsimde Yeni Kanal Oluşturur.
+  **» -giriş-çıkış-ayarla **  Resimli Hg-BB sistemini Açarsınız.
+  **» -giriş-çıkış-kapat **  Ayarlanan Resimli Hg-BB Sistemini Kapatırsınız.
+  **» -güvenlik **  Resimli Güvenlik Sistemini Belirlediğiniz Kanal Yapar..
+  **» -güvenlik sıfırla **   Resimli Güvenlik Sistemini Kapatırsınız.
+  **» -capslock-engelleme **   CapsLock Engelleme Sistemini Açıp Kapatırsınız (İlk Yazışta Açar 2.de kapar)
+  **» -küfürengel**  Küfür Sistemini Açar/Kapatırsınız.
+  **» -reklamengel** Reklam Engel Sistemini Açar/Kapatırsınız.
+  **» -temizle ** Belirlenen Miktarda Mesaj Siler.
+  **» -sunucupanel **   Sunucu Panel Açar.
+  **» -unban ** İdsi Girelen Kullanıcıyı Banının Açar.
+  **» -nuke ** Komutun Yazıldığı Kanalı Silip Aynı İsimde Yeni Kanal Oluşturur.
 
 ▬▬▬▬▬▬▬▬ \`\`\Genel Komutlar\`\`\ ▬▬▬▬▬▬▬▬
 
-**»  ${prefix}davet __Botu Davet Edebilirsiniz!__**
-**»  ${prefix}sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
-**»  ${prefix}istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
-**»  ${prefix}prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
-**»  ${prefix}prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
+**»  -davet __Botu Davet Edebilirsiniz!__**
+**»  -sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
+**»  -istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
+**»  -prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
+**»  -prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
 
 `)
-        .setThumbnail(`https://cdn.discordapp.com/attachments/735925634336817283/762342094554791936/ezgif.com-optimize_8-1.gif`)
-                .addField(`» Wexy Bot Bağlantıları`, `   [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=811238807692771348&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/FQVadvq) **|** [Oy Linki](sadmmsskksks) **|** `)
+        .setThumbnail(``)
+                .addField(`» Jarvıs Bot Bağlantıları`, `   [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=966324540768657478&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/22YqX8sVRg) **|** [Oy Linki](sadmmsskksks) **|** `)
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.displayAvatarURL({dynamic: true}))
     return message.channel.send(embed);
   

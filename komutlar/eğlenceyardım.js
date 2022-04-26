e507716ae84bddf1d7e507a9c02000791316de09
BenZynox <104078510+BenZynox@users.noreply.github.com>
2022-04-26 15:19:59 +0300

    Update eğlenceyardım.js

--- komutlar/eğlenceyardım.js
+++ komutlar/eğlenceyardım.js
@@ -2,40 +2,40 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 const db = require('quick.db');
 const talkedRecently = new Set();
-let botid = ('811238807692771348') 
+let botid = ('966324540768657478') 
 
 exports.run = async(client, message, args) => {
  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!"; 
     const embed = new Discord.MessageEmbed()
         .setAuthor(`${client.user.username} `, client.user.displayAvatarURL({dynamic: true}))
         .setColor('#d02090')
-       .setTitle(`**Wexy Bot  Eğlence Menüsüne Hoşgeldiniz** `)
+       .setTitle(`**Jarvıs Bot  Eğlence Menüsüne Hoşgeldiniz** `)
         .setDescription(`
  
-        **${prefix}çıkma-teklif-et** Etiketlediğiniz Kişiye Çıkma Teklif Edersiniz.
-        **${prefix}tersyazı** Yazdığınız Yazıyı Tersine Çevirir.
-        **${prefix}sarıl**  Etiketlediğiniz Kişiye Sarılırsınız.
-        **${prefix}fbi** FBI Gif Atar
-        **${prefix}espri**  Rastgele Espri Atar.
-        **${prefix}elyazısı**  Yazdığınız Yazıyı El Yazısına Çevirir.
-        **${prefix}doğrulukcesaret**  Doğruluk Veya Cesaret Cümlesi Atar.
-        **${prefix}banner**  Yazdığınız YAZIYI Bannera Çevirir.
-        **${prefix}aşkölçer** Etiketlediğiniz Kişiyle Aranızdaki Aşkı Ölçer. (Şaka Amaçlıdır)
-	    **${prefix}tkm** Bot ile tkm Oynarsınız (t-k-m)
-        **${prefix}ys** Yıldız Savaşı Oynarsınız')
-        **${prefix}bg**  Bilek Güreşi Oynarsınız
-        **${prefix}1vs1**  Düello Oynarsınız
+        **-çıkma-teklif-et** Etiketlediğiniz Kişiye Çıkma Teklif Edersiniz.
+        **-tersyazı** Yazdığınız Yazıyı Tersine Çevirir.
+        **-sarıl**  Etiketlediğiniz Kişiye Sarılırsınız.
+        **-fbi** FBI Gif Atar
+        **-espri**  Rastgele Espri Atar.
+        **-elyazısı**  Yazdığınız Yazıyı El Yazısına Çevirir.
+        **-doğrulukcesaret**  Doğruluk Veya Cesaret Cümlesi Atar.
+        **-banner**  Yazdığınız YAZIYI Bannera Çevirir.
+        **-aşkölçer** Etiketlediğiniz Kişiyle Aranızdaki Aşkı Ölçer. (Şaka Amaçlıdır)
+	    **-tkm** Bot ile tkm Oynarsınız (t-k-m)
+        **-ys** Yıldız Savaşı Oynarsınız')
+        **-bg**  Bilek Güreşi Oynarsınız
+        **-1vs1**  Düello Oynarsınız
 ▬▬▬▬▬▬▬▬ \`\`\Genel Komutlar\`\`\ ▬▬▬▬▬▬▬▬
 
-**»  ${prefix}davet __Botu Davet Edebilirsiniz!__**
-**»  ${prefix}sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
-**»  ${prefix}istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
-**»  ${prefix}prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
-**»  ${prefix}prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
+**»  -davet __Botu Davet Edebilirsiniz!__**
+**»  -sunucutanıt __Sunucunuzu Tanıtabilirsiniz.__**
+**»  -istatistik __Yazarak Botun İstatistiklerini Göre Bilirsiniz.__**
+**»  -prefix __Yazarak Botun Prefixini Değiştirebilirsiniz.__**
+**»  -prefix-sıfırla __Yazarak Ayarladığınız Prefixi Sıfırlayabilirsiniz.__**
 
 `)
-        .setThumbnail(`https://cdn.discordapp.com/attachments/735925634336817283/762342094554791936/ezgif.com-optimize_8-1.gif`)
-                .addField(`» Wexy Bot Bağlantıları`, `   [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=811238807692771348&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/FQVadvq) **|** [Oy Linki](http://bit.ly/Quinoy) **|** `)
+        .setThumbnail(``)
+                .addField(`» Jarvıs Bot Bağlantıları`, `   [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=966324540768657478&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/22YqX8sVRg) **|** [Oy Linki](http://bit.ly/Quinoy) **|** `)
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.displayAvatarURL({dynamic: true}))
     return message.channel.send(embed);
   
